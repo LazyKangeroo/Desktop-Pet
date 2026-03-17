0f2be8f2cf9981a5e118d28fe04256b151c13fa3
LazyKangeroo <fvantonder00@gmail.com>
2026-03-17 21:11:36 +0200

    Entities
    I created classes to hold different entities, cursor, snail information and the window overlay itself.

--- main.py
+++ main.py
@@ -1,29 +1,73 @@
+import random
+import pyautogui
+import time
+
+from enum import Enum
+
 import sys
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
 from PyQt6.QtCore import Qt
 
+# Screen dimentions
+screen_width, screen_height = pyautogui.size()
 
-def main():
+class Directions(Enum):
+    RIGHT = ['1.png', '2.png']
+    LEFT = ['3.png', '4.png']
+    UP =  1
+    DOWN = 2
+
+# ENTITIES
+class Human: # ?Mouse Cursor
+    def __init__(self):
+        pass
+
+class Snail:
+    def __init__(self):
+        self.speed = 2
+        self.pos = {
+            'x' : random.randrange(0, screen_width),
+            'y' : random.randrange(0, screen_height)
+        }
+        self.direction = "None"
+        self.velocty = {
+            'x' : 1,
+            'y' : 1
+        }
+
+    def change_direction(self):
+        pass
+
+    def move(self):
+        pass
+
+    def update(self):
+        pass
+
+# WINDOW
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        # Makes window click through
+        self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
+
+    def display_img(self):
+        label = QLabel(self)
+
+        pixmap = QPixmap('1.png')
+
+        label.setPixmap(pixmap)
+        label.setGeometry(0, 0, pixmap.width(), pixmap.height())
+
+        self.resize(pixmap.width(), pixmap.height())
+
+# RUN
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-
-    window = QWidget()
-
-    window.setWindowFlags(Qt.WindowType.FramelessWindowHint)
-
-    window.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
-    #! Makes window click through
-    window.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
-
-    label = QLabel(window)
-
-    pixmap = QPixmap('1.png')
-    label.setPixmap(pixmap)
-    label.setGeometry(0, 0, pixmap.width(), pixmap.height())
-
-    window.resize(pixmap.width(), pixmap.height())
-    window.show()
-
+    win = Window()
+    win.display_img()
+    win.show()
     sys.exit(app.exec())
-
-main()
